a8456eea68bf93e93961bb49d1b8c368e4b76be9
YonatanLevyn <yonatanlevyn@gmail.com>
2024-02-20 13:34:18 +0200

    Update courses app templates and views

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         return self.queryset.filter(created_by=self.request.user)
