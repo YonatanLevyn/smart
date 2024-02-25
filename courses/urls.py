eff4151f6f8507337e22490a16eb5080d64c36a9
YonatanLevyn <yonatanlevyn@gmail.com>
2024-02-25 22:04:55 +0200

    Updated tests.py

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, LessonViewSet
 
+"""
+    The DefaultRouter automatically generates URL patterns for a given viewset, 
+    covering standard CRUD (Create, Read, Update, Delete) operations. 
+    It uses the information provided by the viewset (such as the queryset 
+    and serializer_class) to determine which actions are available and creates 
+    the appropriate URLs for those actions.
+"""
+
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
+router.register(r'', CourseViewSet)
 router.register(r'lessons', LessonViewSet)
 
 urlpatterns = [
